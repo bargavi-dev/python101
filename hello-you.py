c8e078a4e1c4adaab7462f22a50eee1d7d680e7d
Bargavi  Kanneganti <bargavikanneganti@Bargavis-MBP.attlocal.net>
2020-08-19 16:16:17 -0500

    added all files

---
+++ hello-you.py
@@ -0,0 +1,4 @@
+user_name = input('WHAT IS YOUR NAME?')
+
+print('Hello' + ', ' + user_name + '!')
+print('YOUR NAME HAS ' + str(len(user_name)) + ' LETTERS IN IT! AWESOME!')
---
+++ hello.py
@@ -0,0 +1,11 @@
+
+username = input('Please fill in the blanks below:' +
+'(username)' + "'s" + ' favorite subject in school is (subject).')
+
+username = input('What is the name?')
+print(username)
+subject = input('What is the subject?')
+print(subject)
+print(username + "'s favorite subject in school is " + subject + ".")
+
+
